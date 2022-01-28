b5fb616d35f1a3256b72d9095621596cf8bf34e8
Nic Szerman <nicszerman@gmail.com>
2022-01-28 16:43:50 -0300

    added api default response

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = FastAPI()
 app.mount("/", StaticFiles(directory="web", html=True))
 # html=True automatically loads index.html
 
-# @app.get("/test")
-# async def index():
-#     return {"message": "Hello Shiny Tower test root"}
+@app.get("/api")
+async def api_root():
+    return {"message": "This is Shiny Tower's API :)"}
 
 # @app.get("/")
 # async def root():
