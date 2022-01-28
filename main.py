bde8f899c0b5e24dbbd443b5e4fb83229354b60a
Nic Szerman <nicszerman@gmail.com>
2022-01-28 16:34:47 -0300

    Server working

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/", StaticFiles(directory="web", html=True))
+# html=True automatically loads index.html
 
-@app.get("/")
-async def root():
-    return {"message": "Hello Shiny Tower"}
+# @app.get("/test")
+# async def index():
+#     return {"message": "Hello Shiny Tower test root"}
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello Shiny Tower"}
 
 
